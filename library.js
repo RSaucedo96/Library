c3baed1d31a61a85006254e50bbfe6ee5e2dddb7
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-09-30 12:15:00 -0300

    add book form WIP

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+library = [["The Hobbit", "Tolkien", "168" ,"finished"]];
+
 function Book(title,author,numberPages,state) {
     this.title = title;
     this.author = author;
@@ -8,6 +10,18 @@ function Book(title,author,numberPages,state) {
     };
 }
 
+function addBookToLibrary() {
+    
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  } 
+  
 const theHobbit = new Book("The Hobbit", "Tolkien", "168" ,"finished");
 
 console.log(theHobbit.info());
