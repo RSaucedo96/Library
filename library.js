e4b61adb3f0273eed21beccf8d92b0362bcfaef9
Ricardo <ricardoarielsaucedo96@gmail.com>
2022-10-03 14:44:57 -0300

    added delete button

--- library.js
+++ library.js
@@ -6,13 +6,13 @@ addGameForm.addEventListener("submit", addGameToLibrary);
 displayLibrary();
 
 function Game(title,genre,plataform,state) {
-    this.title = title;
-    this.genre = genre;
-    this.plataform = plataform;
-    this.state = state;
-    this.info=function () {
-        return `Title:${this.title}\nAuthor:${this.plataform}\nNumber of pages:${this.genre}\nState:${this.state}`       
-    };
+  this.title = title;
+  this.genre = genre;
+  this.plataform = plataform;
+  this.state = state;
+  this.info=function () {
+      return `Title:${this.title}\nAuthor:${this.plataform}\nNumber of pages:${this.genre}\nState:${this.state}`       
+  };
 }
 
 function addGameToLibrary(e) {
@@ -30,15 +30,31 @@ function displayLibrary(){
     const newCard = document.createElement("div");
     const newCardTitle = document.createElement("p");
     const newCardData = document.createElement("p");
+    const deleteCard = document.createElement("button")
+    
+    deleteCard.addEventListener('click', function handleClick(event) {
+      event.target.parentElement.remove();
+    });
+    
+    deleteCard.innerHTML = "Delete Game";
+  
     newCard.classList.add("card");
+    newCard.setAttribute('id',`card-${index}`);
+    
     newCardTitle.classList.add("title");
     newCardData.classList.add("data");
+    
     newCardTitle.append(`${library[index].title}`);
     newCardData.append(`Plataform:${library[index].plataform}\nGenre:${library[index].genre}\nState:${library[index].state}`);
-    newCard.appendChild(newCardTitle).appendChild(newCardData);
+   
+    newCard.appendChild(newCardTitle);
+    newCard.appendChild(newCardData);
+    newCard.appendChild(deleteCard);
     gameLibrary.appendChild(newCard);
   }
 }
+
+
 
 function openForm() {
     document.getElementById("addGame-form").style.display = "block";
