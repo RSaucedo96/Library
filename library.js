8849877304bafe2bdc540b94da3c526682a34a48
RSaucedo96 <ricardoarielsaucedo96@gmail.com>
2022-10-05 11:25:13 -0300

    checkbox functionality

--- library.js
+++ library.js
@@ -1,4 +1,4 @@
-library = [{ title: "Spyro", genre: "Plataformer", plataform: "PS1", state: "on" }];
+library = [{ title: "Spyro", genre: "Plataformer", plataform: "PS1", state: true }];
 
 const gameLibrary = document.getElementById("gameLibrary")
 const gameForm = document.getElementById("game-form"); 
@@ -20,8 +20,9 @@ function addGameToLibrary(e) {
   const title = document.querySelector('#title').value;
   const genre = document.querySelector('#genre').value;
   const plataform = document.querySelector('#plataform').value;
+  const state = document.querySelector('#state').checked;
   
-  const game  = new Game(title,genre,plataform);
+  const game  = new Game(title,genre,plataform,state);
   
   library.push(game);
   clearFields()
@@ -34,11 +35,22 @@ function displayLibrary(){
     const newCard = document.createElement("div");
     const newCardTitle = document.createElement("p");
     const newCardData = document.createElement("p");
-    const deleteCard = document.createElement("button")
+    var newCardState = document.createElement("input");
+    const newlabel = document.createElement("label");
+    const deleteCard = document.createElement("button");
     
     deleteCard.addEventListener('click', function handleClick(event) {
       event.target.parentElement.remove();
       library = library.filter(element => element !== library[event.target.parentElement.id]);
+    });
+
+    newCardState.addEventListener('click', function handleClick(event) {
+      if (library[event.target.parentElement.id].state) {
+        library[event.target.parentElement.id].state.checked = false;
+      }
+      else {
+        library[event.target.parentElement.id].state.checked = true;
+      }
     });
     
     deleteCard.innerHTML = "Delete Game";
@@ -49,11 +61,25 @@ function displayLibrary(){
     newCardTitle.classList.add("title");
     newCardData.classList.add("data");
     
+    newCardState.type = 'checkbox';
+    newlabel.setAttribute("for",`${index}`);
+    newlabel.innerHTML = "Finished:";
+    
+    
     newCardTitle.append(`${library[index].title}`);
-    newCardData.append(`Plataform:${library[index].plataform}\nGenre:${library[index].genre}\nState:${library[index].state}`);
-   
+    newCardData.append(`Plataform:${library[index].plataform} \nGenre:${library[index].genre}`);
+    
+    if (library[index].state) {
+      newCardState.checked = true;
+    }
+    else {
+      newCardState.checked = false;
+    }
+
     newCard.appendChild(newCardTitle);
     newCard.appendChild(newCardData);
+    newCard.appendChild(newlabel);
+    newCard.appendChild(newCardState);
     newCard.appendChild(deleteCard);
     gameLibrary.appendChild(newCard);
   }
@@ -64,8 +90,6 @@ function clearFields() {
   document.querySelector('#genre').value='';
   document.querySelector('#plataform').value='';
 }
-
-
 
 function openForm() {
     document.getElementById("game-form").style.display = "block";
